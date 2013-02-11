2319358cf123a37340373c0803426456d4f244a0
Administrator <administrator@Administrators-Mac-mini.local>
2013-02-11 13:58:10 -0700

    Initial Commit

---
+++ AlertsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AlertsTest
+//
+//  Created by Administrator on 13-02-11.
+//  Copyright (c) 2013 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AlertsTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AlertsTest
+//
+//  Created by Administrator on 13-02-11.
+//  Copyright (c) 2013 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
